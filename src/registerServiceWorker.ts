73ab3508db0ae2b4e96f7000e8af20161c88c248
toxic-johann <353904974@qq.com>
2018-09-14 00:33:27 +0800

    fix: remove console

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -89,7 +89,6 @@ function registerValidSW(swUrl: string) {
 }
 
 function checkValidServiceWorker(swUrl: string) {
-  console.error(swUrl)
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
     .then(response => {
